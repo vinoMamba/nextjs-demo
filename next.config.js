8d5b8852ed8773f3871887e8d9f4ede1bd9c0570
vino <vino0908@outlook.com>
2021-05-29 11:09:34 +0800

    添加webpack 配置

---
+++ next.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    webpack: (config, options) => {
+        config.module.rules.push({
+            test: /\.png$/,
+            use: [
+                {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[contenthash].[ext]',
+                        outputPath: 'static', //硬盘路径
+                        publicPath: '_next/static' //网站路径
+                    }
+                },
+            ],
+        })
+        return config
+    },
+}
