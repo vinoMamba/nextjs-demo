2bd186a5edd493456ce8c6db6e6d13c9395678f9
vino <vino0908@outlook.com>
2021-05-29 10:08:48 +0800

    添加全局的Head配置

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+    return (
+        <>
+            <Head>
+                <title>Vino BLog</title>
+                <meta name="viewport"
+                      content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no,viewport-fit=cover"/>
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
 
 export default MyApp
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Link from "next/link";
+import Head from "next/head";
 
 export default function FirstPost() {
     return (
         <>
+            <Head>
+                <title>第一篇博客</title>
+            </Head>
             <h1>First Post</h1>
             <hr/>
             <Link href="/">
