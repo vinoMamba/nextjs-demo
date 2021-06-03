08dfdfed1db5df7acaf337f1d402424d209445c6
vino <vino0908@outlook.com>
2021-06-03 23:46:34 +0800

    添加seed数据

--- dist/seed.js
+++ dist/seed.js
@@ -10,16 +10,47 @@ require("reflect-metadata");
 
 var _typeorm = require("typeorm");
 
+var _User = require("./entity/User");
+
+var _Post = require("./entity/Post");
+
+var _Review = require("./entity/Review");
+
 (0, _typeorm.createConnection)().then( /*#__PURE__*/function () {
   var _ref = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee(connection) {
+    var manager, u1, p1, r1;
     return _regenerator["default"].wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
-            _context.next = 2;
+            manager = connection.manager;
+            u1 = new _User.User();
+            u1.username = "vino";
+            u1.password = "123456";
+            _context.next = 6;
+            return manager.save(u1);
+
+          case 6:
+            p1 = new _Post.Post();
+            p1.author = u1;
+            p1.title = "湖人总冠军";
+            p1.content = "嘻嘻嘻，湖人总冠军";
+            _context.next = 12;
+            return manager.save(p1);
+
+          case 12:
+            r1 = new _Review.Review();
+            r1.content = "太阳队加油，布克牛逼";
+            r1.post = p1;
+            r1.user = u1;
+            _context.next = 18;
+            return manager.save(r1);
+
+          case 18:
+            _context.next = 20;
             return connection.close();
 
-          case 2:
+          case 20:
           case "end":
             return _context.stop();
         }
--- src/seed.ts
+++ src/seed.ts
@@ -1,7 +1,25 @@
 import "reflect-metadata";
 import {createConnection} from "typeorm";
+import {User} from "./entity/User";
+import {Post} from "./entity/Post";
+import {Review} from "./entity/Review";
 
 createConnection().then(async connection => {
+    const manager = connection.manager;
+    const u1 = new User();
+    u1.username = "vino";
+    u1.password = "123456";
+    await manager.save(u1);
+    const p1 = new Post();
+    p1.author = u1;
+    p1.title = "湖人总冠军";
+    p1.content = "嘻嘻嘻，湖人总冠军";
+    await manager.save(p1);
+    const r1 = new Review();
+    r1.content = "太阳队加油，布克牛逼";
+    r1.post = p1;
+    r1.user = u1;
+    await manager.save(r1);
     await connection.close();
 
 }).catch(error => console.log(error));
