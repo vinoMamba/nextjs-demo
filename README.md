812603058b17508bf548111dfb7a2a362403f96a
vino <vino0908@outlook.com>
2021-06-03 22:20:54 +0800

    doc: update README.md

--- README.md
+++ README.md
@@ -13,3 +13,28 @@ yarn dev
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
+## 启动数据库
+
+```bash
+mkdir blog-data
+
+docker run -v "PATH/blog-data":/var/lib/postgresql/data -p 5432:5432 -e POSTGRES_USER=blog -e POSTGRES_HOST_AUTH_METHOD=trust -d postgres
+
+#一些postgres命令
+\l #list database
+\c  # connect to database
+\dt # display table
+```
+
+## 创建数据库
+
+```bash
+# 创建开发环境的数据库
+CREATE DATABASE blog_development ENCODING 'UTF8' LC_COLLATE 'en_US.utf8' LC_CTYPE 'en_US.utf8';
+
+# 创建生产环境的数据库
+CREATE DATABASE blog_production ENCODING 'UTF8' LC_COLLATE 'en_US.utf8' LC_CTYPE 'en_US.utf8';
+
+# 创建测试环境的数据库
+CREATE DATABASE blog_test ENCODING 'UTF8' LC_COLLATE 'en_US.utf8' LC_CTYPE 'en_US.utf8';
+```
--- dist/seed.js
+++ dist/seed.js
@@ -10,42 +10,16 @@ require("reflect-metadata");
 
 var _typeorm = require("typeorm");
 
-var _Post = require("./entity/Post");
-
 (0, _typeorm.createConnection)().then( /*#__PURE__*/function () {
   var _ref = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee(connection) {
-    var posts;
     return _regenerator["default"].wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
             _context.next = 2;
-            return connection.manager.find(_Post.Post);
-
-          case 2:
-            posts = _context.sent;
-
-            if (!(posts.length === 0)) {
-              _context.next = 7;
-              break;
-            }
-
-            _context.next = 6;
-            return connection.manager.save([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map(function (n) {
-              return new _Post.Post({
-                title: "Post ".concat(n),
-                content: "\u4F60\u7684\u6587\u5B57\u6211\u5FC3\u75BC".concat(n)
-              });
-            }));
-
-          case 6:
-            console.log("posts 数据填充了");
-
-          case 7:
-            _context.next = 9;
             return connection.close();
 
-          case 9:
+          case 2:
           case "end":
             return _context.stop();
         }
