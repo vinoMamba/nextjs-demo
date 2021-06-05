6be2c66489902205c7e62d4ae717f548d015d6c0
vino <vino0908@outlook.com>
2021-06-05 16:51:57 +0800

    创建注册页面

---
+++ pages/api/v1/users.tsx
@@ -0,0 +1,18 @@
+import {NextApiHandler} from "next";
+import {getDatabaseConnection} from "../../../lib/getDatabaseConnection";
+import {User} from "../../../src/entity/User";
+
+const Posts: NextApiHandler = async (request, response) => {
+    const {username, password, passwordConfirmation} = request.body;
+    const connection = await getDatabaseConnection();
+    const user = new User();
+    user.username = username;
+    if (password !== passwordConfirmation) {
+        const errors = {passwordConfirmation: ["密码不匹配"]};
+        response.statusCode = 422;
+        response.setHeader("Content-Type", "application/json");
+        response.write(JSON.stringify(errors));
+        response.end();
+    }
+};
+export default Posts;
