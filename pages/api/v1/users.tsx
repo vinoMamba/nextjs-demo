81a7fcbcf5ccccfba9f9a158777ebeea5a34c80c
vino <vino0908@outlook.com>
2021-06-06 18:07:26 +0800

    feat: 注册用户

--- pages/api/v1/users.tsx
+++ pages/api/v1/users.tsx
@@ -1,18 +1,49 @@
 import {NextApiHandler} from "next";
 import {getDatabaseConnection} from "lib/getDatabaseConnection";
 import {User} from "src/entity/User";
+import md5 from "md5";
+import {use} from "marked";
 
 const Posts: NextApiHandler = async (request, response) => {
     const {username, password, passwordConfirmation} = request.body;
-    const connection = await getDatabaseConnection();
-    const user = new User();
-    user.username = username;
-    if (password !== passwordConfirmation) {
-        const errors = {passwordConfirmation: ["密码不匹配"]};
-        response.statusCode = 422;
-        response.setHeader("Content-Type", "application/json");
-        response.write(JSON.stringify(errors));
-        response.end();
+    type Errors = {
+        username: string[];
+        password: string[];
+        passwordConfirmation: string[];
     }
+    const errors: Errors = {username: [], password: [], passwordConfirmation: []};
+    if (username.trim() === "") {
+        errors.username.push("不能为空");
+    }
+    if (!/[a-zA-Z0-9]/.test(username.trim())) {
+        errors.username.push("格式不合法");
+    }
+    if (username.trim().length > 42) {
+        errors.username.push("用户名太长");
+    }
+    if (username.trim().length <= 3) {
+        errors.username.push("用户名太短");
+    }
+    if (password === "") {
+        errors.password.push("密码不能为空");
+    }
+    if (password !== passwordConfirmation) {
+        errors.passwordConfirmation.push("密码不匹配");
+    }
+    const hasErrors = Object.values(errors).find(e => e.length > 0);
+    response.setHeader("Content-Type", "application/json;charset=utf-8");
+    if (hasErrors) {
+        response.statusCode = 422;
+        response.write(JSON.stringify(errors));
+    } else {
+        const connection = await getDatabaseConnection();
+        const user = new User();
+        user.username = username;
+        user.password = md5(password);
+        await connection.manager.save(user);
+        response.statusCode = 200;
+        response.write(JSON.stringify(user));
+    }
+    response.end();
 };
 export default Posts;
