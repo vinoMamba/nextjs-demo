3e14328661b28c69e5115021fa90eae2b1c662b2
vino <vino0908@outlook.com>
2021-06-15 22:12:37 +0800

    feat: 添加环境变量

--- lib/withSession.tsx
+++ lib/withSession.tsx
@@ -3,7 +3,7 @@ import {NextApiHandler} from "next";
 
 export function withSession(handler: NextApiHandler) {
     return withIronSession(handler, {
-        password: "67fc4c7d-7aad-4322-b412-13c6cb82bdee",
+        password: process.env.SECRET as string,
         cookieName: "blog",
         cookieOptions: {
             secure: false
