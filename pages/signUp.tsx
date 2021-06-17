a52e98e5d8cfa7e94432d3ed1f0c17c99edd02f0
vino <vino0908@outlook.com>
2021-06-17 22:32:22 +0800

    feat: SignUp 使用 useForm

--- pages/signUp.tsx
+++ pages/signUp.tsx
@@ -1,21 +1,9 @@
 import {NextPage} from "next";
-import {useCallback, useState} from "react";
 import axios, {AxiosResponse} from "axios";
-import {Form} from "../components/Form";
+import {useForm} from "../hooks/useForm";
 
 const SignUp: NextPage = () => {
-    const [formData, setFormData] = useState({
-        username: "",
-        password: "",
-        passwordConfirmation: ""
-    });
-    const [errors, setErrors] = useState({
-        username: [],
-        password: [],
-        passwordConfirmation: []
-    });
-    const onSubmit = useCallback((e) => {
-        e.preventDefault();
+    const onSubmit = (formData: typeof initFormData) => {
         axios.post(
             `/api/v1/users`,
             formData
@@ -28,45 +16,24 @@ const SignUp: NextPage = () => {
                 setErrors(response.data);
             }
         });
-    }, [formData]);
-    const onChange = useCallback((key: string, value: string) => {
-        setFormData({...formData, [key]: value});
-    }, [formData]);
+    };
+    const initFormData = {
+        username: "",
+        password: "",
+        passwordConfirmation: ""
+    };
+    const {form, setErrors} = useForm({
+        initFormData,
+        fields: [
+            {label: "用户名", type: "text", key: "username"},
+            {label: "密码", type: "password", key: "password"},
+            {label: "确认密码", type: "password", key: "passwordConfirmation"}
+        ],
+        buttons: <button type="submit">注册</button>,
+        onSubmit
+    });
     return (
-        <>
-            <h1>注册</h1>
-            <hr/>
-            <Form
-                onSubmit={onSubmit}
-                fields={[
-                    {
-                        label: "用户名",
-                        type: "text",
-                        value: formData.username,
-                        onChange: e => onChange("username", e.target.value),
-                        errors: errors.username
-                    },
-                    {
-                        label: "密码",
-                        type: "password",
-                        value: formData.password,
-                        onChange: e => onChange("password", e.target.value),
-                        errors: errors.password
-                    },
-                    {
-                        label: "确认密码",
-                        type: "password",
-                        value: formData.passwordConfirmation,
-                        onChange: e => onChange("passwordConfirmation", e.target.value),
-                        errors: errors.passwordConfirmation
-                    },
-                ]}
-                buttons={
-                    <>
-                        <button type="submit">注册</button>
-                    </>
-                }/>
-        </>
+        <>{form}</>
     );
 };
 export default SignUp;
