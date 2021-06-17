3e70ae5af93ad13db001526fdc197a7d129ab925
vino <vino0908@outlook.com>
2021-06-17 22:17:32 +0800

    feat: 创建useForm hooks

--- pages/posts/new.tsx
+++ pages/posts/new.tsx
@@ -2,25 +2,11 @@ import {NextPage} from "next";
 import {Form} from "../../components/Form";
 import {useCallback, useState} from "react";
 import axios, {AxiosResponse} from "axios";
+import {useForm} from "../../hooks/useForm";
 
 const PostsNew: NextPage = () => {
-    const [formData, setFormData] = useState({
-        title: "",
-        content: "",
-    });
-    const onChange = useCallback((key, value) => {
-        setFormData({...formData, [key]: value});
-    }, [formData]);
-    const [errors, setErrors] = useState({
-        title: [],
-        content: [],
-    });
-    const onSubmit = useCallback((e) => {
-        e.preventDefault();
-        axios.post(
-            `/api/v1/posts`,
-            formData
-        ).then((response) => {
+    const onSubmit = (formData: typeof initFormData) => {
+        axios.post(`/api/v1/posts`, formData).then((response) => {
             window.alert("提交成功");
         }, error => {
             const response: AxiosResponse = error.response;
@@ -28,34 +14,23 @@ const PostsNew: NextPage = () => {
                 setErrors(response.data);
             }
         });
-    }, [formData]);
+    };
+    const initFormData = {title: "", content: ""};
+    const {form, setErrors} = useForm(
+        {
+            initFormData,
+            fields: [
+                {label: "标题", type: "text", key: "title"},
+                {label: "内容", type: "textarea", key: "content"}],
+            buttons: <button type="submit">登录</button>,
+            onSubmit
+        }
+    );
+
     return (
-        <>
-            <Form
-                fields={[
-                    {
-                        label: "标题",
-                        type: "text",
-                        value: formData.title,
-                        onChange: e => onChange("username", e.target.value),
-                        errors: errors.title
-                    },
-                    {
-                        label: "内容",
-                        type: "textarea",
-                        value: formData.content,
-                        onChange: e => onChange("password", e.target.value),
-                        errors: errors.content
-                    },
-                ]}
-                onSubmit={onSubmit}
-                buttons={
-                    <>
-                        <button type="submit">登录</button>
-                    </>
-                }
-            />
-        </>
+        <div>
+            {form}
+        </div>
     );
 };
 
