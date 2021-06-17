1e774d97247fe27e1f2697e43b7f3d0b34a5a648
vino <vino0908@outlook.com>
2021-06-17 23:27:41 +0800

    feat: SignUp 优化submit

--- pages/signUp.tsx
+++ pages/signUp.tsx
@@ -3,34 +3,18 @@ import axios, {AxiosResponse} from "axios";
 import {useForm} from "../hooks/useForm";
 
 const SignUp: NextPage = () => {
-    const onSubmit = (formData: typeof initFormData) => {
-        axios.post(
-            `/api/v1/users`,
-            formData
-        ).then((response) => {
-            window.alert("登录成功");
-            window.location.href = "/signIn";
-        }, error => {
-            const response: AxiosResponse = error.response;
-            if (response && response.status === 422) {
-                setErrors(response.data);
-            }
-        });
-    };
-    const initFormData = {
-        username: "",
-        password: "",
-        passwordConfirmation: ""
-    };
     const {form, setErrors} = useForm({
-        initFormData,
+        initFormData: {username: "", password: "", passwordConfirmation: ""},
         fields: [
             {label: "用户名", type: "text", key: "username"},
             {label: "密码", type: "password", key: "password"},
             {label: "确认密码", type: "password", key: "passwordConfirmation"}
         ],
         buttons: <button type="submit">注册</button>,
-        onSubmit
+        submit: {
+            request: formData => axios.post(`/api/v1/users`, formData),
+            message: "注册成功"
+        }
     });
     return (
         <>{form}</>
