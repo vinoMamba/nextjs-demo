14e03700e17f6b9387e03807d75f9058f9353a6c
vino <vino0908@outlook.com>
2021-06-20 16:03:01 +0800

    fix: 添加全局的d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -7,13 +7,6 @@ declare module "*.png" {
     const value: any;
     export default value;
 }
-type Post = {
-    id: string,
-    date: string,
-    title: string,
-    content: string,
-    htmlContent: string
-}
 type UsersErrors = {
     username: string[];
     password: string[];
